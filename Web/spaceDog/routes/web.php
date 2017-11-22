11c45ba14fd77c660543e6d7144e70d313b0f218
Rey Navarro <rey.navarro1996@gmail.com>
2017-11-22 09:00:54 -0700

    First commit, yep, again

---
+++ Web/spaceDog/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('usuario', function () {
+    return view('usuario');
+});
+
+Route::get('entregas', function () {
+    return view('entregas');
+});
+
+Route::get('barcos', function () {
+    return view('barcos');
+});
